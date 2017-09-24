2f838d7337f12c8a41e4f75bfbe5760e972d12dc
Ethan Witherington <wyddindustries@gmail.com>
2017-09-24 19:17:15 -0400

    maintenance push?

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,18 +7,6 @@ import content from 'variable.json';
 import Sector from 'components/Sector'
 import BlockButton from 'components/BlockButton'
 
-/*
-COLOR SCHEME
-DARKS
-#000000 - Black
-#1e1e1e - Slate? Gray
-#37123c - Purple, darkish
-LIGHTS
-#FFFFFF - White
-#87f5fb - Light Blue/Cyan
-#FFb629 - Bitcoin Orange
-*/
-
 class App extends Component {
   render() {
     return (
