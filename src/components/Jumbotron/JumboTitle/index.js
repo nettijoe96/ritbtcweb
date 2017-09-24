336642c8ca0af7fe04ae9197fea7a5a9779afa2b
Ethan Witherington <wyddindustries@gmail.com>
2017-09-24 19:22:41 -0400

    cleaning

---
+++ src/components/Jumbotron/JumboTitle/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Component extends Component {
+  render() {
+    return (
+      <div className="Component">
+      </div>
+    );
+  }
+}
+export default Component;
---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React, { Jumbotron } from 'react';
+import './index.css';
+
+class Jumbotron extends Jumbotron {
+  render() {
+    return (
+      <div className="Jumbotron">
+      </div>
+    );
+  }
+}
+export default Jumbotron;
