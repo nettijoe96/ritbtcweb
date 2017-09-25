c9debac55fa34881c770419752efbe8394c04cdc
Joe Johans <lgvichy@gmail.com>
2017-09-25 19:49:39 -0400

    Merge with master

---
+++ src/components/SampleComponent/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class SampleComponent extends Component {
+  render() {
+    return (
+      <div className="SampleComponent">
+      </div>
+    );
+  }
+}
