88aa04052496681c404b88669a42774dadf7ca3c
Joe Johans <lgvichy@gmail.com>
2017-09-27 23:43:19 -0400

    Adding Cosmos to debug component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './index.css';
 
-class Example extends Component {
+export default class Home extends Component {
   render() {
     return (
-      <div className="Example">
+      <div className="Home">
       </div>
     );
   }
 }
-export default Example;
