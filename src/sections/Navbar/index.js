c21e1feac9b19f28dbb2b5ebf7011b38061c9f5f
Ethan Witherington <wyddindustries@gmail.com>
2017-09-29 20:34:10 -0400

    Jumbotron built

---
+++ src/sections/Navbar/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css';
+import colors from 'colors.json'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div className="Navbar" style={{
+            backgroundColor:colors.dark.off
+            }}>
+                
+            </div>
+        );
+    }
+}
+export default Navbar;
