fecac164af9d8291aea19b2f47117404b2e6f971
Joe Johans <lgvichy@gmail.com>
2017-10-07 01:19:56 -0400

    Done with email field

--- src/components/EmailField/index.jsx
+++ src/components/EmailField/index.jsx
@@ -7,35 +7,56 @@ import emailIcon from './email.svg';
 import sendIcon from './send.svg';
 
 export default class EmailField extends Component {
+	state = {
+		value: ""
+	}
+
+	handleChange = (event) => {
+		this.setState({
+			value: event.target.value
+		});
+	}
+
+	handleSubmit = (event) => {
+		alert('Email entered: ' + this.state.value);
+
+		event.preventDefault();
+	}
+
 	render() {
 		const {
 			placeholder
 		} = this.props;
+
+		const {
+			value
+		} = this.state;;
+
 		return(
 			<div className="EmailField">
-							<header>
-								<h1>Sign Up</h1>
-								<p>Fill in all informations</p>
-							</header>
+				<header>
+					<h1>Sign Up</h1>
+					<p>Fill in all informations</p>
+				</header>
 
-							<form>
+				<form>
+					<div className="EmailSection">
+						<input className="EmailInput" type="email" placeholder={placeholder}
+							value={value} onChange={this.handleChange}/>
 
-								<div className="EmailSection">
-									<input className="EmailInput" type="email" placeholder={placeholder}/>
+						<div className="AnimatedButton">
+							<span className={`${value.length > 0 ? 'Hide' : ''} EmailIconContainer`}>
+								<img src={emailIcon} alt="Email icon" className="EmailIcon"/>
+							</span>
 
-									<div className="AnimatedButton">
-									<span className="EmailIcon">
-										<img src={emailIcon} alt="Email icon"/>
-									</span>
+							<span className="SendButton" onClick={this.handleSubmit}>
+								<img src={sendIcon} alt="Send icon" className="SendIcon"/>
+							</span>
+						</div>
+					</div>
 
-									<span className="SendButton">
-										<img src={sendIcon} alt="Send icon"/>
-									</span>
-									</div>
-								</div>
-
-							</form>
-            </div>
+				</form>
+			</div>
 		);
 	}
 }
